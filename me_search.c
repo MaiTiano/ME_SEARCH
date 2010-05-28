4750944e38a1bb9c326d9bb1fc873371f6aac8ed
MaiTiano <jl@maitian.us>
2010-05-27 21:26:38 -0400

    some comments

--- me_search.c
+++ me_search.c
@@ -47,6 +47,10 @@ unsigned char **out;
 FILE *seqFile;
 FILE *fmv, *fmse; // two files to record the MV and SAD info of searched blocks.
 FILE *p_stat_SAD_MV;// to be used in the function of Stat_calculation() 
+
+
+
+/*A predefined const matrix for MV Euclidean distance*/
 /*
 const int DIST[65]=
 {
